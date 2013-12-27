8adc7ecd8620f94f5ca9687165e88589855a5a0c
Namasteh <risardyo@sinsira.net>
2013-12-26 21:10:55 -0600

    Modified Eve.rb to reflect changes in plugins

--- Eve.rb
+++ Eve.rb
@@ -14,11 +14,14 @@ require_relative "lib/eve/plugins/controlpanel"
 require_relative "lib/eve/plugins/chanopcp"
 require_relative "lib/eve/plugins/privchancp"
 require_relative "lib/eve/plugins/privatecp"
+require_relative "lib/eve/plugins/actai"
+require_relative "lib/eve/plugins/factcore"
+
 
 # You should have something in the block below as it will have Eve report
 # invalid or unauthorized use of her commands to the nicks you place in it
 # Note: Please keep in mind that this doesn't check if the dispatch nick 
-# is authed so if fo some reason it is imperative that no one but the bot
+# is authed so if for some reason it is imperative that no one but the bot
 # masters see output maybe it is wise to only put your nick in here and 
 # make sure no one steals it!
 
@@ -49,7 +52,9 @@ bot = Cinch::Bot.new do
     Cinch::Plugins::ControlPanel,
     Cinch::Plugins::ChanopCP,
     Cinch::Plugins::PrivateCP,
-    Cinch::Plugins::PrivChanCP];
+    Cinch::Plugins::PrivChanCP,
+    Cinch::Plugins::ActAI,
+    Cinch::Plugins::FactCore];
    # In order to make the Twitter plugin work you need to get the api keys
    # from twitter.com. When you have done that and put in the proper keys
    # you can un-comment Cinch::Plugins::Twitter, and the block below, as

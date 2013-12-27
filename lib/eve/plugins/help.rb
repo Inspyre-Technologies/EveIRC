75c29a1cfb7a304deb75b89ce4a8ce20ce4437e4
Namasteh <risardyo@sinsira.net>
2013-12-26 21:10:16 -0600

    Modified help.rb to reflect changes in plugin commands

--- lib/eve/plugins/help.rb
+++ lib/eve/plugins/help.rb
@@ -5,6 +5,8 @@ module Cinch::Plugins
   class Help
     include Cinch::Plugin
     include Cinch::Helpers
+    
+    set :prefix, /^~/
 
     match "help"
   
@@ -12,13 +14,13 @@ module Cinch::Plugins
     if m.channel
       unless check_user(m.user)
         m.user.send Format(:green, "Hello, #{m.user.nick}")
-        m.user.send Format(:green, "You can get further information on a plugin by typing !help <plugin name> IN A PM. Don't do it in a channel!!")
-        m.user.send Format(:orange, "google, memo, seen, urban, eightball, decider")
+        m.user.send Format(:green, "You can get further information on a plugin by typing ~help <plugin name> IN A PM. Don't do it in a channel!!")
+        m.user.send Format(:orange, "google, memo, seen, urban, eightball, decider, factcore")
       return;
     end
         m.user.send Format(:green, "Hello, #{m.user.nick}")
-        m.user.send Format(:green, "You can get further information on a plugin by typing !help <plugin name> IN A PM. Don't do it in a channel!!")
-        m.user.send Format(:orange, "google, memo, seen, urban, eightball, decider")
+        m.user.send Format(:green, "You can get further information on a plugin by typing ~help <plugin name> IN A PM. Don't do it in a channel!!")
+        m.user.send Format(:orange, "google, memo, seen, urban, eightball, decider, factcore")
         m.user.send Format(:red, "The following plugins are only available to operators of the bot! USE WITH CAUTION!! USE OF EVERY ONE OF THESE PLUGINS IS REPORTED!!!")
         m.user.send Format(:red, "channelcp, controlpanel, privatecp, privchancp")
       end

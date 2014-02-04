effbc277b3dd3e11cab69072ff842c3a672fb733
Namasteh <risardyo@sinsira.net>
2014-02-03 22:44:23 -0600

    Added Youtube and fixed Google

---
+++ lib/eve/plugins/you_tube.rb
@@ -0,0 +1,41 @@
+require 'cinch'
+require 'open-uri'
+require 'nokogiri'
+require 'cgi'
+
+module Cinch
+  module Plugins
+    class YouTube
+      include Cinch::Plugin
+      
+      set :prefix, /^~/
+      
+      match /youtube (.+)/
+      
+    def execute(m, query)
+      query.gsub! /\s/, '+'
+      data = lookup(m, query)
+      return m.reply "No results found for #{query}." if data.nil?
+      result(m, data)
+    end
+      
+    def lookup(m, terms)
+      doc = Nokogiri::XML(open("https://gdata.youtube.com/feeds/api/videos?q=#{terms}"))
+      results = doc.css("entry")
+      results.each{|i| puts i.css("title").inner_text()}
+      
+      OpenStruct.new(
+        title1:      results[0].css("title").inner_text(),
+        content1:    results[0].css("content").inner_text(),
+        author1:     results[0].css("author").css("name").inner_text(),
+        url1a:        results[0].css("id").inner_text()
+      )
+      end
+      
+    def result(m, data)
+      url1 = data.url1a.split("http://gdata.youtube.com/feeds/api/videos/")[1]
+      m.reply Format(:green, "%s%s Title: #{data.title1} | #{data.content1} | By: #{data.author1} | http://youtu.be/#{url1}" % [Format(:red, :black, "You")), Format(:red, "Tube")])
+    end  
+  end
+end
+end

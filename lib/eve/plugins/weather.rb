532395c9d6e199a7dc40dddfdc7b7868dbb0d6b0
Namasteh <risardyo@sinsira.net>
2014-02-08 00:48:42 -0600

    Cleaned up weather.rb

--- lib/eve/plugins/weather.rb
+++ lib/eve/plugins/weather.rb
@@ -9,7 +9,12 @@ module Cinch
     class Weather
       include Cinch::Plugin
     
-      set :prefix, /^~/
+      set :required_options, [:key]
+      set :help, <<-USAGE.gsub(/^ {6}/, '')
+        Check the weather right from the IRC channel!
+        Usage:
+        * !weather <location>: The bot will check the weather for the location you provide!
+      USAGE
     
       match /weather (.+)/i, method: :current
     
@@ -21,9 +26,13 @@ module Cinch
         data = get_current(geometry)
         return m.reply 'Uh oh, there was a problem getting the specified weather data. Try again later.' if data.nil?
       
-        m.reply(current_summary(data))
+        locale = location_r(query)
+      
+        m.reply(current_summary(data, locale))
       end
-    
+      
+      # Fetch the location from Google Maps for the area the user is looking for to be passed into get_current.
+      
       def geolookup(zipcode)
         raw_location = JSON.parse(open("http://maps.googleapis.com/maps/api/geocode/json?address=#{zipcode}&sensor=true").read)
         location = raw_location['results'][0]['geometry']['location']
@@ -35,8 +44,11 @@ module Cinch
         nil
       end
       
+      # Fetch the current weather data for the location found in geolookup.
+      
       def get_current(geometry)
-        data = JSON.parse(open("https://api.forecast.io/forecast/9cf5d8e80388a30e7e37b14e43b9707a/#{geometry}").read)
+        key = config[:key]
+        data = JSON.parse(open("https://api.forecast.io/forecast/#{key}/#{geometry}").read)
         current = data['currently']
         
         OpenStruct.new(
@@ -49,9 +61,19 @@ module Cinch
         rescue
           nil
         end
+      
+      # We're gonna fetch the location data again so we can pass it into the results. Primitive, but it works.
+      
+      def location_r(location)
+        raw_locale = JSON.parse(open("http://maps.googleapis.com/maps/api/geocode/json?address=#{location}&sensor=true").read)
+        locale_city = raw_locale['results'][0]['address_components'][1]['long_name']
+        locale_authority = raw_locale['results'][0]['address_components'][3]['long_name']
+        locale_country = raw_locale['results'][0]['address_components'][4]['long_name']
         
+        locale = "#{locale_city}, #{locale_authority}, #{locale_country}"
+      end
     
-      def current_summary(data)
+      def current_summary(data, locale)
         # Converting Imperial for Metric, because it's sane when using a protocol like IRC to deliver this info
       
         temp_c = (data.temp - 32) * 5 / 9
@@ -63,7 +85,7 @@ module Cinch
         
         # Now, deliver the info to IRC
         
-        %Q{Current Weather: #{data.conditions} | Temp: #{data.temp} F (#{temp_c} C) - Feels like: #{data.feels_like} F (#{feels_temp_c} C) | Wind: #{data.wind_speed} MPH (#{wind_speed_kph} KPH) - Bearing: #{data.wind_bearing}  } 
+        %Q{ #{locale} | Current Weather: #{data.conditions} | Temp: #{data.temp} F (#{temp_c} C) - Feels like: #{data.feels_like} F (#{feels_temp_c} C) | Wind: #{data.wind_speed} MPH (#{wind_speed_kph} KPH) - Bearing: #{data.wind_bearing}  } 
       end
     end
   end

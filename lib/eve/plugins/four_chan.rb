f0a587ec6f51e5132254cf92a8e31ff82bf90592
Zach <zgreene02@gmail.com>
2014-04-18 18:07:32 -0400

    Update for 4chan API and structure changes
    Changed /res/ to /reply/ and add semantic URL portion to output

--- lib/eve/plugins/four_chan.rb
+++ lib/eve/plugins/four_chan.rb
@@ -40,6 +40,7 @@
                 replies   = j['replies']
                 images    = j['images']
                 id        = j['no']
+                semantic  = j['semantic_url']
                 
              
                 if subject == nil
@@ -54,7 +55,7 @@
                 comment_s = comment_s[0..200] # We need to limit the characters for 4chan comments
                 
                 if subject.downcase.include? terms.downcase or comment.downcase.include? terms.downcase
-                  results.push("%s /#{board}/ %s - %s (R:%s | I:%s) [ https://boards.4chan.org/#{board}/res/%s ]" % [chan_logo, subject, comment_s, replies, images, id])
+                  results.push("%s /#{board}/ %s - %s (R:%s | I:%s) [ https://boards.4chan.org/#{board}/reply/%s/%s ]" % [chan_logo, subject, comment_s, replies, images, id, semantic])
                 end
               end
             end

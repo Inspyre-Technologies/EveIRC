e30ef7a9dcc730393032b4933768339a002a373c
Zach <zgreene02@gmail.com>
2014-04-18 21:57:21 -0400

    Add ignore

--- lib/eve/plugins/twitter.rb
+++ lib/eve/plugins/twitter.rb
@@ -22,6 +22,7 @@ module Cinch
       match /@(?:-(\d+))?$/, method: :execute_ctweet, prefix: /^~/
 
       def execute_tweet(m, username, offset)
+        return if check_ignore(m.user)
         offset ||= 0
 
         user = @client.user(username)
@@ -46,13 +47,15 @@ module Cinch
 
       match /t-search (.+)/, method: :execute_search, prefix: /^~/
 
-      def execute_search(m, term)    
+      def execute_search(m, term)
+        return if check_ignore(m.user)
         @client.search("#{term}", :result_type => "recent").take(3).each do |tweet|
         m.reply format_tweet(tweet)
       end
     end
 
       def execute_ctweet(m, offset)
+        return if check_ignore(m.user)
         loadfile
         offset ||= 0
 
@@ -96,6 +99,7 @@ module Cinch
       match /#(\d+)$/, method: :execute_id, prefix: /^@/
 
       def execute_id(m, id)
+        return if check_ignore(m.user)
         tweet = @client.status(id)
 
         m.reply format_tweet(tweet)

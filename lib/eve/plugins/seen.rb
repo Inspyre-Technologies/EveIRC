8849e1beeaeb017e8b9a13fe33267541f33277dc
Zach <zgreene02@gmail.com>
2014-04-18 22:02:02 -0400

    Add ignore

--- lib/eve/plugins/seen.rb
+++ lib/eve/plugins/seen.rb
@@ -50,6 +50,7 @@ module Cinch
       private
       
       def seen(nick)
+      	return if check_ignore(m.user)
         @storage[nick.downcase] ||= {}
         seen = @storage[nick.downcase]['seen']
         
@@ -62,6 +63,7 @@ module Cinch
       end
       
       def pm(m)
+       return if check_ignore(m.user)
        return false unless m.channel.nil?
        m.reply 'You must use that command in the main channel.'
        true

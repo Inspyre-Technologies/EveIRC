32b7fd28bfe51aaf1a3af5176892960e4ee3ab84
Richard Banks <namaste@rawrnet.net>
2014-04-18 20:31:01 -0500

    Update ignore_handler.rb
    Fixed errors, cleaned format

--- lib/eve/plugins/ignore_handler.rb
+++ lib/eve/plugins/ignore_handler.rb
@@ -16,8 +16,8 @@ module Cinch
         
       def initialize(*args)
         super
-          if File.exist?('docs/ignorelist.yaml')
-            @storage = YAML.load_file('docs/ignorelist.yaml')
+          if File.exist?('docs/userinfo.yaml')
+            @storage = YAML.load_file('docs/userinfo.yaml')
           else
             @storage = {}
           end
@@ -25,53 +25,52 @@ module Cinch
         
       def reload
         if File.exist?('docs/userinfo.yaml')
-          @storage = YAML.load_file('docs/ignorelist.yaml')
+          @storage = YAML.load_file('docs/userinfo.yaml')
         else
           @storage = {}
       end
     end
+      
+      match /add-ignore (.+)/, method: :add_ignore
+      match /del-ignore (.+)/, method: :del_ignore
     
-    match /add-ignore (.+)/, method: :add_ignore
-    match /del-ignore (.+)/, method: :del_ignore
-    
-    def add_ignore(m, target)
+      def add_ignore(m, target)
         unless check_master(m.user)
           m.user.notice Format(:red, "You are not authorized to use this command! This incident will be reported.")
           Config.dispatch.each { |n| User(n).notice("#{m.user.nick} attempted to use the 'add-ignore' command to add #{target} but was not authorized.") }
-        return;
-      end
-        @storage[User(target).nick] ||= {}
-        @storage[User(target).nick]['ignore'] = true
-        store = @storage[User(target).nick]
-        update_store
-        Config.dispatch.each { |n| User(n).notice("#{m.user.nick} used the 'add-ignore' command to add #{target}.") }
-        m.reply "#{target} added to ignore list."
-      end
-      
-    def del_ignore(m, target)
-          unless check_master(m.user)
-            m.user.notice Format(:red, "You are not authorized to use this command! This incident will be reported.")
-            Config.dispatch.each { |n| User(n).notice("#{m.user.nick} attempted to use the 'del-ignore' command to remove #{target}'s ignore but was not authorized.") }
           return;
         end
-          if @storage.key?(User(target).nick)
-            store = @storage[User(target).nick]
-            store.delete('ignore')
-            update_store
-            Config.dispatch.each { |n| User(n).notice("#{m.user.nick} used the 'del-ignore' command to delete #{target}'s info.") }
-            m.reply "Removed ignore on #{target}."
-          else
-            m.reply "#{target} is not on my ignore list!"
-          end
+          @storage[User(target).nick] ||= {}
+          @storage[User(target).nick]['ignore'] = true
+          update_store
+          Config.dispatch.each { |n| User(n).notice("#{m.user.nick} used the 'add-ignore' command to add #{target}.") }
+          m.reply "#{target} added to ignore list."
+      end
+      
+      def del_ignore(m, target)
+        unless check_master(m.user)
+          m.user.notice Format(:red, "You are not authorized to use this command! This incident will be reported.")
+          Config.dispatch.each { |n| User(n).notice("#{m.user.nick} attempted to use the 'del-ignore' command to remove #{target}'s ignore but was not authorized.") }
+          return;
         end
+        if @storage.key?(User(target).nick)
+          store = @storage[User(target).nick]
+          store.delete('ignore')
+          update_store
+          Config.dispatch.each { |n| User(n).notice("#{m.user.nick} used the 'del-ignore' command to delete #{target}'s info.") }
+          m.reply "Removed ignore on #{target}."
+        else
+          m.reply "#{target} is not on my ignore list!"
+        end
+      end
         
       def update_store
-          synchronize(:update) do
+        synchronize(:update) do
           File.open('docs/ignorelist.yaml', 'w') do |fh|
-          YAML.dump(@storage, fh)
+            YAML.dump(@storage, fh)
+          end
         end
       end
     end
   end
-end
 end

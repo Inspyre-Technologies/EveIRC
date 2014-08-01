7e341bc52e6ac2907943eb48cfa0e3484357c293
Namasteh <namaste@rawrnet.net>
2014-07-31 19:59:02 -0500

    Removed useless code, and stripped custom prefix

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -6,22 +6,19 @@
 # from the plugin itself if needed.
 
 require 'cinch'
-require_relative "config/check_master"
 require_relative "config/check_ignore"
 
 module Cinch::Plugins
   class Help
     include Cinch::Plugin
-    include Cinch::Helpers
 
-    set :prefix, /^~/
-    match "help"
-
-
+    match "!help"
 
     def execute(m)
       list = @bot.plugins.map {|p| p.class.plugin_name}
+
       list = list.sort.join(', ')
+
       m.user.send Format(:green, "Hello, #{m.user.nick}")
       m.user.send Format(:green, "The following is a list of plugins for #{@bot.realname}! To get usage information on a plugin just type ~help <plugin name>")
       m.user.send Format(:orange, "#{list}")

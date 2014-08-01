a7c21cd703be202bc4956b7a5310a467ee4f0e82
Namasteh <namaste@rawrnet.net>
2014-07-31 20:32:42 -0500

    Update

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -11,19 +11,24 @@ require_relative "config/check_ignore"
 module Cinch::Plugins
   class Help
     include Cinch::Plugin
-
-    match "!help"
+    
+    match "help"
 
     def execute(m)
       return if check_ignore(m.user)
 
       list = @bot.plugins.map {|p| p.class.plugin_name}
 
-      list = list.sort.join(', ')
+      for i in list
+	if i !="ai"
 
-      m.user.send Format(:green, "Hello, #{m.user.nick}")
-      m.user.send Format(:green, "The following is a list of plugins for #{@bot.realname}! To get usage information on a plugin just type ~help <plugin name>")
-      m.user.send Format(:orange, "#{list}")
+	  list = i.sort.join(', ')
+
+	  m.user.send Format(:green, "Hello, #{m.user.nick}")
+	  m.user.send Format(:green, "The following is a list of plugins for #{@bot.realname}! To get usage information on a plugin just type ~help <plugin name>")
+	  m.user.send Format(:orange, "#{list}")
+	end
+      end
     end
   end
 end

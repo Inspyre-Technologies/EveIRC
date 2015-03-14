e2b918bcf33cc9b49175f4b7de00d6baa4025295
Richard Banks <risardyo@sinsira.net>
2015-03-14 12:40:57 -0500

    Update constants.rb

--- config/settings/constants.rb
+++ config/settings/constants.rb
@@ -1,7 +1,1 @@
 Cinch::Plugins::ConfigChecks
-Cinch::Plugins::UserInfo
-Cinch::Plugins::Greet
-Cinch::Plugins::AdminHandler
-Cinch::Plugins::BotControl
-Cinch::Plugins::BotInfo
-Cinch::Plugins::ChannelControl

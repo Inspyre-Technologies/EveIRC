fc5f31735f125faeec3688cfe81d8590099bf4df
Richard Banks <namaste@rawrnet.net>
2015-03-14 12:12:51 -0500

    Re-adding config directory

--- config/settings/constants.rb
+++ config/settings/constants.rb
@@ -1,3 +1,7 @@
 Cinch::Plugins::ConfigChecks
+Cinch::Plugins::UserInfo
+Cinch::Plugins::Greet
 Cinch::Plugins::AdminHandler
+Cinch::Plugins::BotControl
 Cinch::Plugins::BotInfo
+Cinch::Plugins::ChannelControl

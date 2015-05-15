cb6ea4249d5b07dee8e005f2b590621d0c9f2a66
Rick Banks <namaste@rawrnet.net>
2015-05-14 21:59:05 -0500

    Refactor | Fixed authentication issues | Added a safe exit method

--- plugins/core/bot_control.rb
+++ plugins/core/bot_control.rb
@@ -1,26 +1,31 @@
-require_relative '../../lib/helpers/identification'
+require_relative '../../lib/helpers/authentication'
+require_relative '../../lib/helpers/quit'
 
 module Cinch
   module Plugins
     class BotControl
       include Cinch::Plugin
-      
+
       set :prefix, eval(YAML.load_file('config/settings/settings.yaml')['prefix'])
-      
-      match /off/i, method: :off 
-      
+
+      match /off/i, method: :off
+
       def off(m)
+        return unless authentication(m)
+        quit_safely
         plugin_name = "BotControl"
         log_message("message", "Received 'off' command from #{m.user.nick}...", plugin_name)
+        exit_normally
         bot.quit("on command of #{m.user.nick}. Exiting normally...")
       end
-      
+
       match /restart/i, method: :restart
-      
+
       def restart(m)
+        return unless authentication(m)
         plugin_name = "BotControl"
         log_message("message", "Received 'restart' command from #{m.user.nick}...", plugin_name)
-        IO.popen("kill #{Process.pid} && ruby Eve.rb&")
+        system("kill #{Process.pid} && ruby Eve.rb")
       end
     end
   end

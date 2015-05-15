89d27b9aff777e3f473b27e5b7e4b90fae0d29a8
Rick Banks <namaste@rawrnet.net>
2015-05-14 21:56:30 -0500

    Removed restore point functon, dangling | Added Core plugins to auto-enabled list

--- lib/utils/plugins_first_run.rb
+++ lib/utils/plugins_first_run.rb
@@ -4,7 +4,6 @@ module Cinch
   module Plugins
     class PluginsFirstRun
       include Cinch::Plugin
-      
       def initialize(*args)
         super
         @compileHash = {} unless @compileHash
@@ -16,19 +15,19 @@ module Cinch
               constant = settings['constant']
               mapping  = settings['file']
               apiURL   = settings['apiURL']
-              
+
               if needKey == true
                 puts "You need to add a key for the #{plugin} plugin. You can register for an API key here: #{apiURL}"
                 puts "Please enter your API key for the #{plugin} plugin."
-                
+
                 key = gets
                 key = key.chomp
-                
+
                 puts "Thank you! Your API key for the #{plugin} plugin is recorded as #{key}"
               end
-              
+
               filePath = "require_relative '../../plugins/#{mapping}'"
-              
+
               compileOptions = @compileHash[plugin] ||= {}
               compileOptions['constant'] = constant
               compileOptions['mapping']  = filePath
@@ -42,19 +41,25 @@ module Cinch
           sleep config[:delay] || 2
         end
       end
-      
+
       def core_plugins
         log_message("message", "Preparing to write core plugins...")
         log_message("message", "Writing core plugins to plugins file...")
         open('config/settings/plugins.rb', 'a') {|f|
           f.puts "require_relative '../../plugins/core/admin_handler'"
           f.puts "require_relative '../../plugins/core/bot_info'"
+          f.puts "require_relative '../../plugins/core/bot_control'"
+          f.puts "require_relative '../../plugins/core/channel_control'"
+          f.puts "require_relative '../../plugins/core/update'"
         }
         log_message("message", "Core plugins written to plugins file! Proceeding...")
         log_message("message", "Writing core plugins to constants file...")
         open('config/settings/constants.rb', 'a') {|f|
           f.puts "Cinch::Plugins::AdminHandler"
           f.puts "Cinch::Plugins::BotInfo"
+          f.puts "Cinch::Plugins::BotControl"
+          f.puts "Cinch::Plugins::ChannelControl"
+          f.puts "Cinch::Plugins::Update"
         }
         log_message("message", "Core plugins written to constants file! Core plugins written.")
         log_message("message", "Registering plugins with the bot...")
@@ -63,27 +68,27 @@ module Cinch
         @bot.plugins.register_plugin(Cinch::Plugins::BotInfo)
         log_message("message", "Core plugins registered with bot!")
       end
-    
+
       def update_plugins(data)
         log_message("message", "Creating plugins file...")
         File.new('config/settings/plugins.rb', 'w')
         log_message("message", "Created!")
-        
+
         data = data.each do |plugin, settings|
           constant = settings['constant']
           mapping  = settings['mapping']
-          
+
           log_message("message", "Preparing #{plugin}...")
-          
+
           log_message("message", "Writing #{plugin} to plugins file...")
           open('config/settings/plugins.rb', 'a') { |f| f.puts "#{mapping}" }
-          
+
           log_message("message", "Writing #{plugin} to constant file...")
           open('config/settings/constants.rb', 'a') { |f| f.puts "#{constant}" }
-          
+
           log_message("message", "Registering #{plugin}...")
           load 'config/settings/plugins.rb'
-          
+
           begin
             constant = Cinch::Plugins.const_get(constant)
             @bot.plugins.register_plugin(constant)
@@ -100,33 +105,20 @@ module Cinch
         puts "Fantastic! Starting bot..."
         sleep config[:delay] || 2
       end
-      
-      def create_restore
-        log_message("message", "Creating bot restore point...")
-        FileUtils.cp_r('', 'lib/restore/', :verbose => true)
-        log_message("message", "Bot restore point created!")
-      end
-      
+
       def update_main
         data = "require_relative 'config/settings/plugins.rb'"
         new_main = File.new("Eve1.rb", 'w')
         new_main.puts(data)
-        
+
         old_main = File.open("Eve.rb", 'r+')
         old_main.each_line { |line| new_main.puts line }
-        
+
         new_main.close()
         old_main.close()
-        
-        begin
-          File.rename("Eve.rb", "lib/restore/Eve.rb")
-          log_message("message", "Restore point created, can be found in '~/lib/restore/Eve.rb'")
-        rescue
-          File.delete("Eve1.rb")
-          log_message("error", "There seems to have been an issue creating the restore point: #{$!}")
-          abort("There was an error creating restore point. Aborting.")
-        end
-        
+
+        File.delete("Eve.rb")
+
         File.rename("Eve1.rb", "Eve.rb")
       end
     end

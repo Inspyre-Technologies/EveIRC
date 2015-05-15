14e4d72d5e43bc8b7f2553ba17123c17e2e26beb
Rick Banks <namaste@rawrnet.net>
2015-05-14 21:58:25 -0500

    Refactor | Fixed lack of nil rescue for user_info.rb

--- plugins/greet.rb
+++ plugins/greet.rb
@@ -1,44 +1,39 @@
 require 'cinch'
 require_relative '../lib/helpers/logger'
+require_relative '../lib/helpers/file_handler'
 
 module Cinch
   module Plugins
     class Greet
       include Cinch::Plugin
       include Cinch::Helpers
-      
-      PluginName = "Greet"
-      
+
+      Plugin_Name = "Greet"
+
       def initialize(*args)
         super
-        if File.exist?('config/settings/user_info.yaml')
-          log_message("message", "Loading user_info.yaml...", PluginName)
-          user_settings = YAML.load_file('config/settings/user_info.yaml')
-          log_message("message", "user_info.yaml loaded successfully!", PluginName)
-          log_message("message", "Creating array from user_info.yaml contents", PluginName)
-          @userNicks = user_settings
-          log_message("message", "Array created!", PluginName)
+        if $user_file
+          log_message("message", "user_info.yaml loaded successfully!", Plugin_Name)
         else
-          log_message("warn", "No user_info.yaml file found. Continuing without it...", PluginName)         
+          log_message("warn", "No user_info.yaml file found. Continuing without it...", Plugin_Name)
         end
       end
-      
+
       listen_to :join, :method => :greet
-      
+
       def greet(m)
-        reload
-        if @userNicks.key?(m.user.nick.downcase) and @userNicks[m.user.nick.downcase].key?('greeting')
-          greeting = @userNicks[m.user.nick.downcase]['greeting']
-          m.reply "#{greeting}"
-        else
+        return m.reply "Hello #{m.channel}!" if m.user.nick == bot.nick
+        if !$user_file
           m.reply "Hello, #{m.user.nick}!"
+        else
+          if $user_file.key?(m.user.nick.downcase) and $user_file[m.user.nick.downcase].key?('greeting')
+            greeting = $user_file[m.user.nick.downcase]['greeting']
+            m.reply "#{greeting}"
+          else
+            m.reply "Hello, #{m.user.nick}!"
+          end
         end
       end
-      
-      def reload
-        @userNicks = YAML.load_file('config/settings/user_info.yaml')
-      end
-      
     end
   end
 end

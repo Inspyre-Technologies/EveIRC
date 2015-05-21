f88ca96b0656d502de06f41ab88ba1343b62d1ea
Rick Banks <namaste@rawrnet.net>
2015-05-21 02:00:29 -0500

    Fixed gsub issue that was removing numbers from location strings

--- lib/helpers/geolocation.rb
+++ lib/helpers/geolocation.rb
@@ -4,11 +4,11 @@ require_relative 'logger'
 
 module Cinch
   module Helpers
-    
+
     def geolocation(location)
       begin
         log_message("message", "Looking for #{location}...")
-        formatted_location = location.gsub! /\s/, '+'
+        formatted_location = location.gsub /\s/, '+'
         geo_page = JSON.parse(open("http://maps.googleapis.com/maps/api/geocode/json?address=#{formatted_location}&sensor=true").read)
         result = geo_page['results'][0]['geometry']['location']
         @lat = result['lat']

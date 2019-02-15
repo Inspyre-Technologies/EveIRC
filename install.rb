3a4a3fc46429f11ff8d5d5bd03278be7123dff81
Taylor B <tayjaybabee@gmail.com>
2019-02-15 00:21:18 -0600

    Added function to create userinfo.yaml if not found

--- install.rb
+++ install.rb
@@ -1,8 +1,24 @@
-
+require 'yaml'
 # The script is for easy install of the gems Eve requires
 
+def initialize(*args)
+super
+  if File.exist?('docs/userinfo.yaml')
+    @storage = YAML.load_file('docs/userinfo.yaml')
+  else
+    @storage = {}
+    puts"Creating user config file..."
+    system("touch docs/userinfo.yaml")
+    puts"User config file added..."
+  end
+end
 
-
+def update_store
+  say "Updating storage with master information..."
+  File.open('docs/userinfo.yaml', 'w') do |fh|
+    YAML.dump(@storage, fh)
+  end
+end
 
 # This function checks if a gem is installed and installs it if it isn't
 def install_check(gem)
@@ -31,14 +47,28 @@ def install_highline
   end
 end
 
+def create_userfile
+  puts "What is your IRC nickname? "
+  answer = gets.chomp
+  puts "Thank you, #{answer}"
+  @storage = {}
+  @storage[answer] ||= {}
+  @storage[answer]['adminLevel'] = 0
+  puts "Writing to config file..."
+end
+
+create_userfile
+
 install_highline
 
 require 'highline/import'
 cli = HighLine.new
+update_store
 system("clear")
 
 # Start main script, ask if they wanna update their gems
 say "Welcome to eve6!"
+
 if agree("Would you like to run an update on your Ruby Gems? [y/n]: ", true)
   system("gem update")
   say("Your gems have been updated!")
@@ -65,7 +95,6 @@ say "Using Bundler to install needed gems..."
 system *%W[bundle]
 
 say "Required dependencies for eve6 now installed."
-
 say "Once you've configured Eve you can start it using: ruby Eve.rb"
 
 system("kill #{Process.pid}") # End program
